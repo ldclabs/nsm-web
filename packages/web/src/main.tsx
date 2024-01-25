2d00eb43cbf5f05f87c4b4ee0a651be93457d986
0xZensh <txr1883@gmail.com>
2024-01-25 12:09:14 +0800

    fix: try to fix "cannot-destructure-property" error on `react-router`

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,5 +1,5 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 if ('serviceWorker' in navigator) {
@@ -13,7 +13,7 @@ if ('serviceWorker' in navigator) {
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 )
